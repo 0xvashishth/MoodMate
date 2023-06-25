83d5501039673f0fbbe92446f0721fed661b59d5
Vashishth Patel <vashishthchaudhary48@gmail.com>
2023-06-25 17:19:14 +0000

    controller added

---
+++ MoodMateServer/MoodMateServer/Controllers/MoodMateController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoodMateServer.Hubs;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.AspNetCore.Http.Connections;
+
+namespace MoodMateServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoodMateController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            int TotalConnections = 1;
+            return "value";
+        }
+
+        private async Task<int> GetConnectedUsersCount()
+        {
+            var connection = new HubConnectionBuilder()
+                .Build();
+
+            await connection.StartAsync();
+
+            try
+            {
+                return await connection.InvokeAsync<int>("SendAllCurrentUsers");
+            }
+            finally
+            {
+                await connection.DisposeAsync();
+            }
+        }
+
+
+    }
+}
