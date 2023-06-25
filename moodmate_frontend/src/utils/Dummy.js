f164988b81493a719bc5e4f2248a62021a6cb514
Prince <pmdhamecha673@gmail.com>
2023-06-25 21:11:54 +0530

    Mood choices reduced for now

--- moodmate_frontend/src/utils/Dummy.js
+++ moodmate_frontend/src/utils/Dummy.js
@@ -1,32 +1,10 @@
 import happy from "../utils/gifs/happy.gif";
 import angry from "../utils/gifs/angry.gif";
-import frustrated from "../utils/gifs/frustrated.gif";
 import sad from "../utils/gifs/sad.gif";
-import sick from "../utils/gifs/sick.gif";
-import confused from "../utils/gifs/confused.gif";
-import amazed from "../utils/gifs/amazed.gif";
-import excited from "../utils/gifs/excited.gif";
-import tired from "../utils/gifs/tired.gif";
 
 export let gifObj = {
   0: happy,
-  1: amazed,
-  2: excited,
-  3: confused,
-  4: angry,
-  5: frustrated,
-  6: sad,
-  7: sick,
-  8: tired,
+  1: angry,
+  2: sad,
 };
-export let moodVal = [
-  "happy",
-  "amazed",
-  "excited",
-  "confused",
-  "angry",
-  "frustrated",
-  "sad",
-  "sick",
-  "tired",
-];
+export let moodVal = ["happy", "angry", "sad"];
