0f4aa94b48d90fb78507edc0b0fcb58b4777ab8c
Prince <pmdhamecha673@gmail.com>
2023-06-26 00:39:46 +0530

    joined message ui updated

--- moodmate_frontend/src/Components/MoodMate.jsx
+++ moodmate_frontend/src/Components/MoodMate.jsx
@@ -3,10 +3,10 @@ import { BsRobot } from "react-icons/bs";
 
 const MoodMate = ({ msg }) => {
   return (
-    <div className="text-center mb-4">
+    <div className="text-center mb-4 w-3/4 m-auto h-fit">
       <div className="w-fit flex justify-center flex-row m-auto bg-white p-3 px-6 rounded-lg gap-3">
         <BsRobot className="my-auto block" />
-        <p className="w-fit bg-white rounded-md">{msg}</p>
+        <p className="w-fit bg-white rounded-md whitespace-pre-line">{msg}</p>
       </div>
     </div>
   );
